0efc150b1920c59a5f53ef9eb10b80e2d544d080
Hamza Qureshi <hamza.qureshi97@gmail.com>
2018-09-25 13:07:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string){
   if (string.toLowerCase() === lowercase){
     return "I can\'t hear you!"
   }
-  else if (string === uppercase) {
+  else if (string.toUpperCase() === uppercase ) {
     return "YES INDEED!"
   }
   else if (string === "I love you, Grandma.") {
