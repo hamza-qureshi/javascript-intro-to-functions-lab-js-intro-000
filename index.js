46116aefa36638345699b7fe5e48710206f34f56
Hamza Qureshi <hamza.qureshi97@gmail.com>
2018-09-25 12:53:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout (string){
+  return string.toUpperCase()
+}
+
+
+function whisper (string){
+  return string.toLowerCase()
+}
+
+
+function logShout(string){
+  console.log(shout());
+}
+
+
+function logShout(string){
+  console.log(whisper());
+}
+
+
+
+function sayHiToGrandma(string){
+  if (string === uppercase){
+    return "I can\'t hear you!"
+  }
+  else if (string === lowercase) {
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
